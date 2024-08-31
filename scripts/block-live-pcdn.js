77c16380565f78acb3bec4ba62ac71eea147f183
qianxu <qianxuuuu@qq.com>
2024-09-01 01:32:05 +0800

    chore: 更新脚本描述

--- scripts/block-live-pcdn.js
+++ scripts/block-live-pcdn.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        禁止直播平台 PCDN 上传
-// @description 禁止直播平台 PCDN 上传
+// @description 防止直播平台浪费带宽
 // @author      qianxu
 // @version     1.1.0
 // @match       https://*.huya.com/*
