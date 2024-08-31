a946337ab47a19ff4ccbc9c6d29eb0701be5cad0
qianxu <qianxuuuu@qq.com>
2024-09-01 01:23:13 +0800

    chore: 更新脚本描述

--- scripts/bilibili-auto-redirect-to-blanc-live-room.js
+++ scripts/bilibili-auto-redirect-to-blanc-live-room.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        哔哩哔哩自动跳转到纯净版直播间
-// @description 哔哩哔哩自动跳转到纯净版直播间
+// @description 无各种活动的直播间
 // @author      qianxu
 // @version     1.1.0
 // @match       https://live.bilibili.com/*
 // @icon        https://www.bilibili.com/favicon.ico
-// @namespace   https://greasyfork.org/scripts/473292
+// @namespace   bilibili-auto-redirect-to-blanc-live-room
 // @grant       none
 // @license     MIT
 // ==/UserScript==
