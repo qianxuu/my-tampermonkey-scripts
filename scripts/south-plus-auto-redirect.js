d15e5e2dc72c051f063a7f62047be864465bf62f
qianxu <qianxuuuu@qq.com>
2024-12-16 22:54:46 +0800

    feat: 南 + 自动跳转脚本添加更多镜像域名并优化跳转逻辑

--- scripts/south-plus-auto-redirect.js
+++ scripts/south-plus-auto-redirect.js
@@ -2,7 +2,7 @@
 // @name        南 + 自动跳转到主域名
 // @description 避免需要重复登录，自动跳转到主域名桌面版
 // @author      qianxu
-// @version     1.1.1
+// @version     1.2.0
 // @match       https://*.north-plus.net/*
 // @match       https://*.south-plus.org/*
 // @match       https://*.spring-plus.net/*
@@ -10,6 +10,7 @@
 // @match       https://*.south-plus.net/*
 // @match       https://*.east-plus.net/*
 // @match       https://*.blue-plus.net/*
+// @match       https://*.snow-plus.net/*
 // @icon        https://south-plus.net/favicon.ico
 // @namespace   south-plus-auto-redirect
 // @license     MIT
@@ -17,13 +18,14 @@
 // ==/UserScript==
 
 const { hostname, pathname, search } = location
-if (hostname !== 'south-plus.net') {
-  location.hostname = 'south-plus.net'
-}
 
-// 如果 pathname 含有 simple 则为移动版，跳转到桌面版
+// 如果 pathname 含有 simple 则为移动版，直接跳转到主域名桌面版，否则只替换为主域名
 if (pathname.includes('simple')) {
-  // 从 search 取出帖子 ID
-  const id = search.match(/\d+/)[0]
-  location.href = `https://south-plus.net/read.php?tid=${id}`
+  // 从 search 中取出帖子 ID
+  const match = search.match(/\d+/)
+  if (match) {
+    location.href = `https://south-plus.net/read.php?tid=${match[0]}`
+  }
+} else if (hostname !== 'south-plus.net') {
+  location.hostname = 'south-plus.net'
 }
