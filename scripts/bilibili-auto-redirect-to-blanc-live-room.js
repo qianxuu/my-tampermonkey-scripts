3144edb6f72ff05ec97b15b5d41c649130c1bf5b
qianxu <qianxuuuu@qq.com>
2025-03-01 22:37:05 +0800

    perf: 提早脚本运行时机

--- scripts/bilibili-auto-redirect-to-blanc-live-room.js
+++ scripts/bilibili-auto-redirect-to-blanc-live-room.js
@@ -2,8 +2,9 @@
 // @name        哔哩哔哩直播自动跳转到纯净版直播间
 // @description 自动跳转到无各种活动的直播间
 // @author      qianxu
-// @version     1.1.1
+// @version     1.1.2
 // @match       https://live.bilibili.com/*
+// @run-at      document-start
 // @icon        https://www.bilibili.com/favicon.ico
 // @namespace   bilibili-auto-redirect-to-blanc-live-room
 // @license     MIT
