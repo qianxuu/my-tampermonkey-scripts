0454a68b509b0c8742b2ef827724986841d98fdf
qianxu <qianxuuuu@qq.com>
2026-03-30 17:20:22 +0800

    更新阻止 PCDN 脚本

--- scripts/live-pcdn-blocker.js
+++ scripts/live-pcdn-blocker.js
@@ -1,19 +1,17 @@
 // ==UserScript==
-// @name        禁止直播平台 PCDN 上传
+// @name        阻止直播平台 PCDN 上传
 // @description 阻止直播平台占用上行带宽和消耗流量
 // @author      qianxu
-// @version     1.1.2
+// @version     1.2.0
 // @match       https://*.huya.com/*
 // @match       https://*.douyu.com/*
 // @match       https://live.bilibili.com/*
 // @icon        https://www.huya.com/favicon.ico
-// @namespace   block-live-pcdn
+// @namespace   live-pcdn-blocker
 // @license     MIT
 // @grant       none
 // ==/UserScript==
 
 (() => {
-	window.RTCDataChannel = undefined;
-	window.RTCPeerConnection = undefined;
-	window.webkitRTCPeerConnection = undefined;
+	window.RTCDataChannel.prototype.send = () => {};
 })();
